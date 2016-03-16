cab0fa90ecbdc53f2459514962ad2ca03eb51c2e
Kiea Seok Kang <grtlinux@gmail.com>
2016-03-17 08:36:21 +0900

    .

--- DEPLOY_01/libs/make.cmd
+++ DEPLOY_01/libs/make.cmd
@@ -22,10 +22,16 @@ jar xvf ..\tain-deploy-1.0.jar
 
 jar cvf ..\commons-net-3.3.0.jar META-INF org log4j.properties
 
+::-------------------------------------------
+:: rmdir
+
+cd ..
+rmdir /S /Q JOB
 
 ::-------------------------------------------
 :: finish
 :END
 
-pause
+timeout 1
+::pause
 
